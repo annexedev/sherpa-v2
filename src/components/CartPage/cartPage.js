73e017c40707771afe77a23302c127c709406b5d
Alana Moraes <amoraes@gticanada.com>
2024-02-14 15:33:58 -0500

    button quantity change show only if open

--- src/components/CartPage/cartPage.js
+++ src/components/CartPage/cartPage.js
@@ -473,7 +473,7 @@ const CartPage = props => {
                                                             <h1 className={classes.headingProductsWithProject}>
                                                                 <ProjectName cid={Object.keys(products)[0]} />
                                                             </h1>
-                                                            <div className={classes.ctaUpdate}><a href={"/myprojects?id=" + Object.keys(products)[0]}>Update your project items / quantity <FontAwesomeIcon icon={faLongArrowAltRight} style={{ color: "#8DC74C", marginLeft: "10px", }} /></a></div>
+                                                          {openProjects[projectId] &&  <div className={classes.ctaUpdate}><a href={"/myprojects?id=" + Object.keys(products)[0]}>Update your project items / quantity <FontAwesomeIcon icon={faLongArrowAltRight} style={{ color: "#8DC74C", marginLeft: "10px", }} /></a></div>}
                                                             <div className={classes.wrapperValeurProduits}>
                                                                 <span><CountProjectItem products={itemsWithProject} inputCategory={Object.keys(products)[0]} /></span>
                                                                 <span className={classes.circleIcon}><FontAwesomeIcon icon={faCircle} /></span>
